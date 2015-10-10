7773775b7084d81a8806cdb18336e634e33477f6
Katherine Sullivan <katms729@gmail.com>
2015-10-10 04:09:56 -0400

    section tracks hours and days separately

--- src/main/java/parser/Section.java
+++ src/main/java/parser/Section.java
@@ -6,20 +6,31 @@ import org.jsoup.select.Selector;
 
 public class Section {
     public Section(Element elem) {
-        time = Selector.select(ID.sectionTime, elem).get(0).ownText();
+        String[] temp = parseTime(Selector.select(ID.sectionTime, elem).get(0).ownText());
+        days = temp[0];
+        time = temp[1];
         room = Selector.select(ID.sectionRoom, elem).get(0).ownText();
         instructor = Selector.select(ID.instructor, elem).get(0).ownText();
         nbr = Selector.select(ID.sectionNbr, elem).get(0).ownText();
     }
 
     public String toString() {
-        return nbr+" "+time+" "+room+" "+instructor;
+        return nbr+" "+days+" "+time+" "+room+" "+instructor;
     }
 
-
+    //return strings representing the days and the hours
+    private String[] parseTime(String secTime) {
+        if(secTime.equals("TBA")) {
+            return new String[] {"TBA", "TBA"};
+        }
+        else {
+            return secTime.split(" ", 2);
+        }
+    }
 
     public final String nbr;
     public final String time;
+    public final String days;
     public final String room;
     public final String instructor;
 }
