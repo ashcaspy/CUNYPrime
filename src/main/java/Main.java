7bb5b1e7450a444ba880e8423306d8fe7864ce06
Katherine Sullivan <katms729@gmail.com>
2015-10-11 15:25:40 -0400

    cleanup main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 
 import java.io.IOException;
 import java.sql.DriverManager;
-import java.util.HashMap;
 import java.util.List;
 
 import com.gargoylesoftware.htmlunit.html.*;
@@ -18,33 +17,26 @@ class Main {
             wc.setup("Hunter College", ID.semester("Fall", 2015));
 
             List<HtmlOption> depts = wc.getSelect(cunyfirst.ID.selectDept).getOptions();
+            Connection conn;
+            String serverName = "localhost:3306";
+            String mydatabase = "cunyfirst";
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+            try {
+                conn = DriverManager.getConnection(url, args[0], args[1]);
 
             //the first option is blank
-            HashMap<String, Parser> departments = new HashMap<>(depts.size()-1);
-            for(HtmlOption o: depts.subList(1,depts.size())) {
+            for (HtmlOption o : depts.subList(1, depts.size())) {
                 String deptCode = o.getValueAttribute();
                 if(ID.skippedDepts.contains(deptCode)) {
 
                     continue;
                 }
-                departments.put(deptCode, new Parser(wc.getResults(deptCode).asXml()));
+                new Parser(wc.getResults(deptCode).asXml()).addToTable(conn);
             }
 
-            try {
-                String serverName = "localhost:3306";
-                String mydatabase = "cunyfirst";
-                String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
-
-                Connection conn = DriverManager.getConnection(url, args[0], args[1]);
-
-                for(Parser p: departments.values()) {
-                    p.addToTable(conn);
-                }
-
-                conn.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+    }
 }
