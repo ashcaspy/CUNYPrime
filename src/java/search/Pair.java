8703ec04b1d6b6e1c6dcb45ea76ffcba626bbb58
GracieCoding <grace.liang33@myhunter.cuny.edu>
2015-11-21 01:23:22 -0500

    Update

--- src/java/search/Pair.java
+++ src/java/search/Pair.java
@@ -2,16 +2,26 @@
  * Created by gracie on 11/19/2015.
  */
 public class Pair {
-    final int x;
-    final int y;
+    int x;
+    int y;
 
+    /**
+     * Constructor setting default values.
+     */
     Pair(){
-        x = 0;
-        y= 0;
+        x = -2;
+        y= -2;
     }
+
+    /**
+     * A Constructor that takes in two integer values and stores them as a pair.
+     * @param x, An integer value representing one value of the pair.
+     * @param y, An integer value representing the other value of the pair.
+     */
     Pair(int x, int y){
         this.x = x;
         this.y = y;
     }
+
 
 }
--- src/java/search/Schedule.java
+++ src/java/search/Schedule.java
@@ -16,11 +16,60 @@ public class Schedule {
         mySchedule = new ArrayList<>();
     }
 
+
     /**
-     * A function that sets the open time slots to values given by an array.
-     * @param openTimeSlot, An array containing values of open time slots.
+     * A construtor that takes in two String arrays, and sets the member variables according to values in the arrays.
+     * @param openTimes, A string array containing open time slots for selected days.
+     * @param closedTimes, A string array containing unavailable time slots for selected days.
      */
-    public void setOpenTimes(String [] openTimeSlot){
+    public Schedule(String [] openTimes, String [] closedTimes){
+        mySchedule = new ArrayList<>();
+        String temp[];
+        int x = -1;
+        Day day = new Day();
+        mySchedule.add(day);
+        int prevY = 0;
+        for (int i = 0; i < openTimes.length; i++){
+            temp = openTimes[i].split("-");
+            if(temp.length > 0) {
+                if(mySchedule.get(mySchedule.size() -1).getDay() != Integer.parseInt(temp[2])){
+                    if(i != 0){
+                        day = new Day();
+                        mySchedule.add(day);
+                    }
+                    mySchedule.get(mySchedule.size() - 1).setDay(Integer.parseInt(temp[2]));
+
+                }
+                if(x == -1){
+                    mySchedule.get(mySchedule.size() -1).addToOpenTimes(new Pair());
+                    if(i == 0){
+                        mySchedule.get(mySchedule.size() - 1).getOpenTimeElement(mySchedule.get(mySchedule.size() -1).getOpenTimeSize() - 1).x = Integer.parseInt(temp[3]);
+                        x = 0;
+
+                    } else {
+                        if (Math.abs(Integer.parseInt(temp[3]) - mySchedule.get(mySchedule.size() -1).getOpenTimeElement(mySchedule.get(mySchedule.size() - 1).getOpenTimeSize() - 2).y) > 1) {
+                            mySchedule.get(mySchedule.size() - 1).getOpenTimeElement(mySchedule.get(mySchedule.size() - 1).getOpenTimeSize() - 1).x = Integer.parseInt(temp[3]);
+                            x = 0;
+
+                        } else if (Math.abs(Integer.parseInt(temp[3]) - mySchedule.get(mySchedule.size() -1).getOpenTimeElement(mySchedule.get(mySchedule.size() -1).getOpenTimeSize() - 2).y) == 1) {
+                            mySchedule.get(mySchedule.size() - 1).getOpenTimeElement(mySchedule.get(mySchedule.size() -1).getOpenTimeSize() - 1).x = prevY;
+                            mySchedule.get(mySchedule.size() - 1).getOpenTimeElement(mySchedule.get(mySchedule.size() -1).getOpenTimeSize() - 1).y =  Integer.parseInt(temp[3]);
+                            prevY = Integer.parseInt(temp[3]);
+                        }
+                    }
+
+                } else if(x == 0){
+                    mySchedule.get(mySchedule.size() - 1).getOpenTimeElement(mySchedule.get(mySchedule.size() - 1).getOpenTimeSize() - 1).y  = Integer.parseInt(temp[3]);
+                    prevY = Integer.parseInt(temp[3]);
+                    x = -1;
+                }
+            }
+        }
+
+        /*for (int i = 0; i < mySchedule.size();i++){
+            mySchedule.get(i).print();
+        }*/
+
 
     }
 }
