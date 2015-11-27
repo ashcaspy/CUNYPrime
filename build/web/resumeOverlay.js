1230f657c0d6a10b337d8ee11324013fe3b4e54f
Ashley Foglia <ashley.foglia@yahoo.com>
2015-11-27 14:53:29 -0500

    Implementing Resume Builder
    Added embedded pdf generation and display.

--- build/web/resumeOverlay.js
+++ build/web/resumeOverlay.js
@@ -1,16 +1,60 @@
-function changeName(){
-    $("#resume_name_reflection").html($("#resume_name").val());
+// PDF GENERATION
+function initPDF(){
+    var printDoc = new jsPDF();
+    $("#pdf_view_pane").attr("data", printDoc.output("datauristring"));
 }
 
-function changeAddress(){
-    $("#resume_address_reflection").html($("#resume_address").val());
+function generatePDF(){
+    var printDoc = new jsPDF();
+    //printDoc.fromHTML($('#display_pane').get(0), 10, 10, {'width': 180});
+    //printDoc.autoPrint();
+    printDoc.text(20, 20, $("#resume_name").val());
+    printDoc.text(20, 30, $("#resume_address").val());
+    var stateString = "";
+    if ($("#resume_state").val() != "")
+        stateString = ", ";
+    
+    printDoc.text(20, 40, $("#resume_city").val() + stateString+ $("#resume_state").val());
+    printDoc.text(20, 50, $("#resume_zip").val());
+    $("#pdf_view_pane").attr("data", printDoc.output("datauristring"));
+    
 }
 
-function changeState(){
-    $("#resume_state_reflection").html($("#resume_state").val());
+
+// ADD NEW EDUCATION/WORK
+var workCounter = 0;
+var educationCounter = 0;
+function addEducation(){
+    var $newEducation = $("<div>").addClass("resume_input_div");
+    $newEducation.attr("id", "resume_education_" + educationCounter);
+    $newEducation.html(
+        "College Name:<input id = \"resume_education_where_" + educationCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" + 
+        "Graduation Year:<input id = \"resume_education_year_" + educationCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" +
+        "Degree:<input id = \"resume_education_degree_" + educationCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" +
+        "Additional Info:<input id = \"resume_education_info_" + educationCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />"
+    );
+    
+    $("#resume_education").append($("<div>").addClass("resume_filler").html("<b>Education " + (educationCounter+1) + ":</b>"));
+    $("#resume_education").append($newEducation);
+    educationCounter++;
+    
 }
 
-function changeCity(){
-    $("#resume_city_reflection").html($("#resume_city").val() + ",&nbsp;" + $("#resume_state").val());
+function addWork(){
+    var $newWork = $("<div>").addClass("resume_input_div");
+    $newWork.attr("id", "resume_education_" + workCounter);
+    $newWork.html(
+        "<b>Employer:</b><input id = \"resume_work_where_" + workCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" +
+        "<b>Dates:</b><input id = \"resume_work_year_" + workCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" +
+        "<b>Position:</b><input id = \"resume_work_position_" + workCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />" +
+        "<b>Responsibilities:</b><input id = \"resume_work_info_" + workCounter + "\" maxlength = 50 onblur = \"javascript:generatePDF();\"></input><br />"
+    );
+    
+    $("#resume_work").append($("<div>").addClass("resume_filler").html("<b>Work Experience " + (workCounter+1) + ":</b>"));
+    $("#resume_work").append($newWork);
+    workCounter++;
 }
+
+
+// EXPAND AND COLLAPSE DIVS (MAYBE ABANDON THIS)
 
--- src/java/connections/MainConfig.java
+++ src/java/connections/MainConfig.java
@@ -16,16 +16,15 @@ import java.sql.SQLException;
 public class MainConfig {
 
     public static Connection getConnection() throws URISyntaxException, SQLException, ClassNotFoundException {
-        
+     
     // Heroku connection
-    
+
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
- 
-        
-    
+
+       
         Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(dbUrl, username, password);
     }
