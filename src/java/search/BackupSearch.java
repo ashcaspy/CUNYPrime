909bf76a3833f2a667371f95fd425120a795988d
Katherine Sullivan <katms729@gmail.com>
2015-12-06 04:34:49 -0500

    check departments

--- src/java/search/BackupSearch.java
+++ src/java/search/BackupSearch.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Collectors;
 import search.cunyfirst.MatchValuePair;
 import search.parser.Section;
 
@@ -35,8 +36,20 @@ public class BackupSearch extends Search {
         PreparedStatement insert;
         try {
             Section.createTable(conn, offset());
-            insert = conn.prepareStatement("INSERT INTO " + tableName() + 
-                    "SELECT * FROM " + masterTable);
+            String depts;
+            if(null == departments) {
+                // instead of checking whether to use WHERE or not
+                // let this evaluate to true every time
+                depts = "cdept LIKE '%'";
+            } else {
+                // quote all departments and arrange them as ('x', 'y', ...)
+                depts = "cdept IN (" + String.join(",", departments.stream()
+                                .map(d -> "'"+d+"'").collect(Collectors.toList())) +
+                                ")";
+            }
+
+            insert = conn.prepareStatement("INSERT INTO " + tableName() + " SELECT * FROM "+ masterTable +
+                    " WHERE " + depts + ";");
             insert.execute();
             
         } catch (SQLException e) {
